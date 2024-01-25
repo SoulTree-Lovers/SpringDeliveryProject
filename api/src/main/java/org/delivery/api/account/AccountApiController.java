d0d369a9809a2dad16d17120b90e6a8a273ce3a8
kangseungmin <rkdtmdals999@naver.com>
2024-01-25 10:43:14 +0900

    [feat] API에 공통 스펙 적용하기

--- api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -3,6 +3,7 @@ package org.delivery.api.account;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.account.model.AccountMeResponse;
+import org.delivery.api.common.api.Api;
 import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +17,13 @@ public class AccountApiController {
 
     private final AccountRepository accountRepository;
     @GetMapping("/me")
-    public AccountMeResponse me() {
-        return AccountMeResponse.builder()
+    public Api<AccountMeResponse> me() {
+        var response = AccountMeResponse.builder()
                 .name("강승민")
                 .email("kang@naver.com")
                 .registeredAt(LocalDateTime.now())
                 .build();
+
+        return Api.OK(response);
     }
 }
