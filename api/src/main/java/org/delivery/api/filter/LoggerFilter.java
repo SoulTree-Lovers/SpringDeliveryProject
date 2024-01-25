62c3c46be9d1602e5014b81484d5e05b4b7efd2c
kangseungmin <rkdtmdals999@naver.com>
2024-01-25 10:21:04 +0900

    [feat] Filter를 통해 Request, Response log 예쁘게 출력하기

---
+++ api/src/main/java/org/delivery/api/filter/LoggerFilter.java
@@ -0,0 +1,76 @@
+package org.delivery.api.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+@Slf4j
+@Component
+public class LoggerFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        // 버퍼에서 한 번 읽을 때 버퍼의 값이 삭제되는 것을 막도록 래퍼 클래스로 감싸기
+        var req = new ContentCachingRequestWrapper((HttpServletRequest) request);
+        var res = new ContentCachingResponseWrapper((HttpServletResponse) response);
+
+        /** 실행 전 */
+
+        chain.doFilter(req, res);
+
+        /** 실행 후 */
+        // request 정보 로그 출력
+        var headerNames = req.getHeaderNames();
+        var headerValues = new StringBuilder();
+
+        headerNames.asIterator().forEachRemaining(headerKey -> {
+            var headerValue = req.getHeader(headerKey);
+
+            // authorization-token : ??? , user-agent : ??? , ...
+            headerValues
+                    .append("[")
+                    .append(headerKey)
+                    .append(" : ")
+                    .append(headerValue)
+                    .append("] ");
+
+        });
+
+        var requestBody = new String(req.getContentAsByteArray());
+        var uri = req.getRequestURI();
+        var method = req.getMethod();
+
+        log.info(">>>>> uri : {}, method : {}, header : {}, body : {}", uri, method, headerValues, requestBody);
+
+        // response 정보 로그 출력
+        var responseHeaderValues = new StringBuilder();
+
+        res.getHeaderNames().forEach(headerKey -> {
+            var headerValue = res.getHeader(headerKey);
+
+            responseHeaderValues
+                    .append("[")
+                    .append(headerKey)
+                    .append(" : ")
+                    .append(headerValue)
+                    .append("] ");
+        });
+
+        var responseBody = new String(res.getContentAsByteArray());
+
+        log.info("<<<<< uri : {}, method : {}, header : {}, body : {}", uri, method, responseHeaderValues, responseBody);
+
+        res.copyBodyToResponse();
+    }
+}
