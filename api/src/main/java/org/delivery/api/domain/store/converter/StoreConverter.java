b6ad7c9ea601dfd1d9dce7e1198fccfe77d72bdd
kangseungmin <rkdtmdals999@naver.com>
2024-01-27 16:33:52 +0900

    [feat] Store Controller, Service, Converter, Model 구현

---
+++ api/src/main/java/org/delivery/api/domain/store/converter/StoreConverter.java
@@ -0,0 +1,52 @@
+package org.delivery.api.domain.store.converter;
+
+import java.util.Optional;
+import javax.swing.text.html.Option;
+import org.delivery.api.common.annotation.Converter;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.exception.ApiException;
+import org.delivery.api.domain.store.controller.model.StoreRegisterRequest;
+import org.delivery.api.domain.store.controller.model.StoreResponse;
+import org.delivery.db.store.StoreEntity;
+
+@Converter
+public class StoreConverter {
+
+    public StoreEntity toEntity(StoreRegisterRequest request) {
+
+        return Optional.ofNullable(request)
+                .map(it -> {
+                    return StoreEntity.builder()
+                            .name(request.getName())
+                            .address(request.getAddress())
+                            .category(request.getCategory())
+                            .minimumAmount(request.getMinimumAmount())
+                            .minimumDeliveryAmount(request.getMinimumDeliveryAmount())
+                            .thumbnailUrl(request.getThumbnailUrl())
+                            .phoneNumber(request.getPhoneNumber())
+                            .build()
+                            ;
+                })
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT));
+    }
+
+    public StoreResponse toResponse(StoreEntity storeEntity) {
+
+        return Optional.ofNullable(storeEntity)
+                .map(it -> {
+                    return StoreResponse.builder()
+                            .id(storeEntity.getId())
+                            .name(storeEntity.getName())
+                            .status(storeEntity.getStatus())
+                            .address(storeEntity.getAddress())
+                            .category(storeEntity.getCategory())
+                            .star(storeEntity.getStar())
+                            .thumbnailUrl(storeEntity.getThumbnailUrl())
+                            .minimumAmount(storeEntity.getMinimumAmount())
+                            .minimumDeliveryAmount(storeEntity.getMinimumDeliveryAmount())
+                            .phoneNumber(storeEntity.getPhoneNumber())
+                            .build();
+                })
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT));
+    }
+}
---
+++ api/src/main/java/org/delivery/api/domain/store/service/StoreService.java
@@ -0,0 +1,53 @@
+package org.delivery.api.domain.store.service;
+
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.exception.ApiException;
+import org.delivery.db.store.StoreEntity;
+import org.delivery.db.store.StoreRepository;
+import org.delivery.db.store.enums.StoreCategory;
+import org.delivery.db.store.enums.StoreStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StoreService {
+
+    private final StoreRepository storeRepository;
+
+
+    // 유효한 스토어 검색
+    public StoreEntity getStoreWithThrow(Long id) {
+        var entity = storeRepository.findFirstByIdAndStatusOrderByIdDesc(id, StoreStatus.REGISTERED); // 디버깅을 위해 변수로 구분하기.
+        return entity.orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT));
+    }
+
+    // 스토어 등록
+    public StoreEntity register(StoreEntity storeEntity) {
+        return Optional.ofNullable(storeEntity)
+                .map(it -> {
+                    it.setStar(0);
+                    it.setStatus(StoreStatus.REGISTERED);
+
+                    // TODO: 등록 일시 추가하기
+
+                    return storeRepository.save(it);
+                })
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT));
+    }
+
+    // 카테고리로 스토어 검색
+    public List<StoreEntity> searchByCategory(StoreCategory storeCategory) {
+        var list = storeRepository.findAllByStatusAndCategoryOrderByStarDesc(StoreStatus.REGISTERED, storeCategory);
+        return list;
+    }
+
+    // 전체 스토어 검색
+    public List<StoreEntity> searchAll() {
+        var list = storeRepository.findAllByStatusOrderByIdDesc(StoreStatus.REGISTERED);
+        return list;
+    }
+
+}
