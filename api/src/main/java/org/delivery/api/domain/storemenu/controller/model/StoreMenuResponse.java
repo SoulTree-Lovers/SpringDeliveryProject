344ffa2f3207503a1b503e411f7f16bcaf696ef1
kangseungmin <rkdtmdals999@naver.com>
2024-01-27 17:59:43 +0900

    [feat] Store Menu 서비스 로직 개발 - Controller, Business, Service, Converter

---
+++ api/src/main/java/org/delivery/api/domain/storemenu/controller/model/StoreMenuResponse.java
@@ -0,0 +1,32 @@
+package org.delivery.api.domain.storemenu.controller.model;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.delivery.db.storemenu.enums.StoreMenuStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StoreMenuResponse {
+
+    private Long id;
+
+    private Long storeId;
+
+    private String name;
+
+    private BigDecimal amount;
+
+    private StoreMenuStatus status;
+
+    private String thumbnailUrl;
+
+    private int likeCount;
+
+    private int sequence;
+
+}
