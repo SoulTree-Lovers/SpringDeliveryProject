9d07e2a6666ae7f41752a4ed07124961571f4574
kangseungmin <rkdtmdals999@naver.com>
2024-01-27 15:22:52 +0900

    [feat] Store DB 개발 및 Entity, Repository 연동

---
+++ db/src/main/java/org/delivery/db/store/StoreRepository.java
@@ -0,0 +1,22 @@
+package org.delivery.db.store;
+
+import java.util.List;
+import java.util.Optional;
+import org.delivery.db.store.enums.StoreCategory;
+import org.delivery.db.store.enums.StoreStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<StoreEntity, Long> {
+
+    // 스토어 검색 (id + store)
+    // select * from store where id = ? and status = ? order by id desc limit 1
+    Optional<StoreEntity> findFirstByIdAndStatusOrderByIdDesc(Long id, StoreStatus status);
+
+    // 유효한 스토어 리스트
+    // select * from store where status = ? order by id desc
+    List<StoreEntity> findAllByStatusOrderByIdDesc(StoreStatus status);
+
+    // 유효한 특정 카테고리의 스토어 리스트 (별점을 기준으로 정렬)
+    List<StoreEntity> findAllByStatusAndCategoryOrderByStarDesc(StoreStatus status, StoreCategory category);
+
+}
