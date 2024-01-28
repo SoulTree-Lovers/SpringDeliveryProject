1f49fd673bac6a95111ab756450a068780c24cab
kangseungmin <rkdtmdals999@naver.com>
2024-01-28 11:42:22 +0900

    [feat] 주문 내역 및 메뉴에 대한 쿼리 메소드 작성, Service 로직 구현

---
+++ api/src/main/java/org/delivery/api/domain/userorder/service/UserOrderService.java
@@ -0,0 +1,91 @@
+package org.delivery.api.domain.userorder.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.exception.ApiException;
+import org.delivery.db.userorder.UserOrderEntity;
+import org.delivery.db.userorder.UserOrderRepository;
+import org.delivery.db.userorder.enums.UserOrderStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserOrderService {
+
+    private final UserOrderRepository userOrderRepository;
+
+
+    public List<UserOrderEntity> getUserOrderList(Long userId) {
+        return userOrderRepository.findAllByUserIdAndStatusOrderByIdDesc(userId, UserOrderStatus.REGISTERED);
+    }
+
+    public List<UserOrderEntity> getUserOrderList(Long userId, List<UserOrderStatus> statusList) {
+        return userOrderRepository.findAllByUserIdAndStatusInOrderByIdDesc(userId, statusList);
+    }
+
+    public UserOrderEntity getUserOrderWithThrow(Long id, Long userId) {
+        return userOrderRepository.findAllByIdAndStatusAndUserId(id, UserOrderStatus.REGISTERED, userId)
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT));
+    }
+
+    // 현재 진행 중인 내역 가져오기
+    public List<UserOrderEntity> current(Long userId) {
+        return getUserOrderList(userId, List.of(
+                UserOrderStatus.ORDER,
+                UserOrderStatus.COOKING,
+                UserOrderStatus.DELIVERY,
+                UserOrderStatus.ACCEPT
+        ));
+    }
+
+    // 과거 주문한 내역 가져오기
+    public List<UserOrderEntity> history(Long userId) {
+        return getUserOrderList(userId, List.of(
+                UserOrderStatus.RECEIVE
+        ));
+    }
+
+    // 주문
+    public UserOrderEntity order(UserOrderEntity entity) {
+        return Optional.ofNullable(entity)
+                .map(it -> {
+                    it.setStatus(UserOrderStatus.ORDER);
+                    it.setOrderedAt(LocalDateTime.now());
+                    return userOrderRepository.save(it);
+                })
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT));
+    }
+
+    // 상태 변경
+    public UserOrderEntity setStatus(UserOrderEntity entity, UserOrderStatus status) {
+        entity.setStatus(status);
+        return userOrderRepository.save(entity);
+    }
+
+    // 주문 확인
+    public UserOrderEntity accept(UserOrderEntity entity) {
+        entity.setAcceptedAt(LocalDateTime.now());
+        return setStatus(entity, UserOrderStatus.ACCEPT);
+    }
+
+    // 조리 시작
+    public UserOrderEntity cooking(UserOrderEntity entity) {
+        entity.setCookingStartedAt(LocalDateTime.now());
+        return setStatus(entity, UserOrderStatus.COOKING);
+    }
+
+    // 배달 시작
+    public UserOrderEntity delivery(UserOrderEntity entity) {
+        entity.setDeliveryStartedAt(LocalDateTime.now());
+        return setStatus(entity, UserOrderStatus.DELIVERY);
+    }
+
+    // 배달 완료
+    public UserOrderEntity receive(UserOrderEntity entity) {
+        entity.setReceivedAt(LocalDateTime.now());
+        return setStatus(entity, UserOrderStatus.RECEIVE);
+    }
+}
--- db/src/main/java/org/delivery/db/userorder/enums/UserOrderStatus.java
+++ db/src/main/java/org/delivery/db/userorder/enums/UserOrderStatus.java
@@ -6,6 +6,11 @@ import lombok.AllArgsConstructor;
 public enum UserOrderStatus {
     REGISTERED("등록"),
     UNREGISTERED("해지"),
+    ORDER("주문"),
+    ACCEPT("확인"),
+    COOKING("조리 중"),
+    DELIVERY("배달 중"),
+    RECEIVE("배달 완료"),
     ;
 
     private final String description;
