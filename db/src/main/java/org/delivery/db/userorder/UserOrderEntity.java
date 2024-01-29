a4d48857642f81241600c8a94f02d758cb3f385b
kangseungmin <rkdtmdals999@naver.com>
2024-01-29 22:06:28 +0900

    [feat] UserOrder table과 Store table N:1 매핑을 위해 storeId 멤버변수 추가

--- db/src/main/java/org/delivery/db/userorder/UserOrderEntity.java
+++ db/src/main/java/org/delivery/db/userorder/UserOrderEntity.java
@@ -27,6 +27,9 @@ public class UserOrderEntity extends BaseEntity {
     @Column(nullable = false)
     private Long userId; // user 테이블과 1:n 매핑
 
+    @Column(nullable = false)
+    private Long storeId; // store 테이블과 1:n 매핑
+
     @Enumerated(EnumType.STRING)
     @Column(length = 50, nullable = false, columnDefinition = "varchar")
     private UserOrderStatus status;
